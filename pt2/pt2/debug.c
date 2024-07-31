1bac049dd1e9a7ca59e8ab255ae5ecb9958b8b24
Cam Scherman <camscherman@hotmail.com>
2024-07-30 20:18:13 -0700

    finish basic VM

--- pt2/pt2/debug.c
+++ pt2/pt2/debug.c
@@ -20,9 +20,9 @@ static int simpleInstruction(const char *name, int offset)
 static int constantInstruction(const char *name, Chunk *chunk, int offset)
 {
   uint8_t constant = chunk->code[offset + 1];
-  printf("%-16s %4d", name, constant);
+  printf("%-16s %4d '", name, constant);
   printValue(chunk->constants.values[constant]);
-  printf("\n");
+  printf("'\n");
   return offset + 2;
 }
 
@@ -31,7 +31,7 @@ int disassembleInstruction(Chunk *chunk, int offset)
   printf("%04d ", offset);
   if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1])
   {
-    printf("    |  ");
+    printf("   | ");
   }
   else
   {
@@ -43,6 +43,16 @@ int disassembleInstruction(Chunk *chunk, int offset)
   {
   case OP_CONSTANT:
     return constantInstruction("OP_CONSTANT", chunk, offset);
+  case OP_NEGATE:
+    return simpleInstruction("OP_NEGATE", offset);
+  case OP_ADD:
+    return simpleInstruction("OP_ADD", offset);
+  case OP_SUBTRACT:
+    return simpleInstruction("OP_SUBTRACT", offset);
+  case OP_MULTIPLY:
+    return simpleInstruction("OP_MULTIPLY", offset);
+  case OP_DIVIDE:
+    return simpleInstruction("OP_DIVIDE", offset);
   case OP_RETURN:
     return simpleInstruction("OP_RETURN", offset);
   default:
